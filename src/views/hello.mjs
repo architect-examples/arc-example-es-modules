49e1e8bbd6b98af846d5f07eb8a513ca23bf03b1
kj <kristoferjoseph@gmail.com>
2018-11-12 17:05:38 -0800

    Adds main.mjs and load pattern.

--- src/views/hello.mjs
+++ src/views/hello.mjs
@@ -1,5 +1,7 @@
-(function () {
+function Message () {
   console.log('@dam💕')
   let message = document.getElementById('js-message')
   message.innerHTML = 'You are now rocking with ES Modules.'
-}())
+}
+
+export default Message
---
+++ src/views/main.mjs
@@ -0,0 +1,2 @@
+import Message from './hello.mjs'
+Message()
